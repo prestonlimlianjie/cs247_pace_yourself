36a8bbc39ebfb65e203e410eff77d9d6b96fbe49
Preston Lim <prestonlimlianjie@gmail.com>
2017-03-14 20:29:05 -0700

    Add files via upload

---
+++ components/hostatalk/hostatalkController.js
@@ -0,0 +1,8 @@
+'use strict';
+
+cs142App.controller('HostATalkController', ['$scope', '$routeParams', '$resource', '$rootScope', '$route', '$location',
+  function ($scope, $routeParams, $resource, $rootScope, $route, $location) {
+
+
+    };
+}]);
