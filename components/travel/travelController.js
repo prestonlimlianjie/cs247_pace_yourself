0608ac339adb92dbe5472a3bbedfe73f21697bf3
Preston Lim <prestonlimlianjie@gmail.com>
2017-03-15 23:55:45 -0700

    Add files via upload

---
+++ components/travel/travelController.js
@@ -0,0 +1,21 @@
+'use strict';
+
+cs142App.controller('travelController', ['$scope', '$routeParams', '$resource', '$rootScope', '$route', '$location',
+  function ($scope, $routeParams, $resource, $rootScope, $route, $location) {
+  	$scope.main.title = $scope.main.username + '\'s Favorites';
+
+    $scope.likeButtonClick = function(photoObj_id){
+    	console.log(photoObj_id);
+
+    	var favoriteRes = $resource('/favoritePhoto/' + photoObj_id);
+    	var photoModel = favoriteRes.save({}, function(){
+        console.log("Favorite button clicked");
+        $route.reload();
+
+    	}, function errorHandling(err){
+        console.log('Problem retrieving photoModel');
+        return;
+    	});
+
+    };
+}]);
